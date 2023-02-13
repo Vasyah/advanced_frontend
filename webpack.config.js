a55241b803024c0cae6c2c341f5975e6aec90738
20002477 <VVasiGoydin@sberbank.ru>
2023-02-13 18:12:09 +0300

    - add HtmlWebpackPlugin - add ProgressPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require('webpack'); //to access built-in plugins
 
 
 // аналог js экспорта, только для node
@@ -8,8 +10,18 @@ module.exports = {
 
 	output: {
 		filename: "bundle.js",
-		path: path.resolve(__dirname, 'build')
+		path: path.resolve(__dirname, 'build'),
+		// очистка build папки перед сборкой нового
+		clean: true
 	},
+
+	plugins: [
+		// плагин для создания html файла с подключенными скриптами
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, 'public', 'index.html')
+		}),
+		new webpack.ProgressPlugin(),
+	],
 
 	mode: 'development'
 }

f6b5647ec8e774999a4bd0d31e7f9d4450e7c375
Vasyah <Souji_933@mail.ru>
2024-06-25 20:49:21 +0300

    webpack config

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -6,21 +6,29 @@ import { type BuildOptions } from '../types/config'
 
 export const buildPlugins = (
 	options: BuildOptions,
-): webpack.WebpackPluginInstance[] => [
+): webpack.WebpackPluginInstance[] => {
+	const plugins = [
 	// плагин для создания html файла с подключенными скриптами
-	new HtmlWebpackPlugin({
-		template: options.paths.html,
-	}),
-	// отображает процесс сборки в процентах
-	new webpack.ProgressPlugin(),
-	new MiniCssExtractPlugin({
+		new HtmlWebpackPlugin({
+			template: options.paths.html,
+		}),
+		// отображает процесс сборки в процентах
+		new webpack.ProgressPlugin(),
+		new MiniCssExtractPlugin({
 		// при асинхронной подгрузки
-		chunkFilename: 'css/[name].[contenthash:8].css',
-		filename: 'css/[name].[contenthash:8].css',
-	}),
-	// https://webpack.js.org/plugins/define-plugin/
-	new webpack.DefinePlugin({
-		__IS_DEV__: JSON.stringify(options.isDev),
-	}), new webpack.HotModuleReplacementPlugin(),
-	new BundleAnalyzerPlugin({ openAnalyzer: false }),
-]
+			chunkFilename: 'css/[name].[contenthash:8].css',
+			filename: 'css/[name].[contenthash:8].css',
+		}),
+		// https://webpack.js.org/plugins/define-plugin/
+		new webpack.DefinePlugin({
+			__IS_DEV__: JSON.stringify(options.isDev),
+		}), new webpack.HotModuleReplacementPlugin(),
+		new BundleAnalyzerPlugin({ openAnalyzer: false }),
+	]
+
+	if (options?.isDev) {
+		plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }))
+	}
+
+	return plugins
+}
